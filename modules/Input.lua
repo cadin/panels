79b78c05ef73fc7a3fa4bf07ba7b9522da85b933
Cadin Batrack <cabatrac@microsoft.com>
2021-08-12 11:35:17 -0700

    add Input constants

---
+++ modules/Input.lua
@@ -0,0 +1,8 @@
+Panels.Input = {
+	A = playdate.kButtonA,
+	B = playdate.kButtonB,
+	UP = playdate.kButtonUp,
+	DOWN = playdate.kButtonDown,
+	LEFT = playdate.kButtonLeft,
+	RIGHT = playdate.kButtonRight
+}
