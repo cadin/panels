2897840589f97292cd21fc3ffdcaa85eaf1aa3dc
Cadin Batrack <cabatrac@microsoft.com>
2021-08-11 19:54:35 -0700

    add round util

---
+++ modules/Utils.lua
@@ -0,0 +1,5 @@
+function round(num, numDecimalPlaces)
+	local mult = 10^(numDecimalPlaces or 0)
+	if num >= 0 then return math.floor(num * mult + 0.5) / mult
+	else return math.ceil(num * mult - 0.5) / mult end
+end
